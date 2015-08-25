f76bb523351b6e06d48fb469e150b0a7c1d4f50c
Heather Baldwin <h.baldwin07@gmail.com>
2015-08-25 12:30:27 -0400

    Python code and answer for question 1

---
+++ day2/day2-lunch/question1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+
+filename = "/Users/cmdb/qbb2015/day1/SRR072893.sam"
+
+f = open(filename)
+
+
+line_count = 0
+for line in f: 
+    if "@" in line:
+        pass
+    else:
+        line_count += 1
+print line_count
