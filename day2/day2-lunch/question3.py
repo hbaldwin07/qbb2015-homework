0776927ec3b2c8cdf9caa8be495595f8b78ef1ce
Heather Baldwin <h.baldwin07@gmail.com>
2015-08-25 13:13:55 -0400

    Answers for question 3

---
+++ day2/day2-lunch/question3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+
+filename = "/Users/cmdb/qbb2015/day1/SRR072893.sam"
+
+f = open(filename)
+
+
+line_count = 0
+
+for line in f:
+    if "@" in line:
+        pass
+    elif "NH:i:1" in line:
+        line_count += 1
+print line_count
