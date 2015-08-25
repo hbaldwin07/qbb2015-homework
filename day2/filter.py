c0f90feda68172957ccca8358e5bcd6e0ad53765
Heather Baldwin <h.baldwin07@gmail.com>
2015-08-25 11:24:29 -0400

    Filter lines 11-15

--- day2/filter.py
+++ day2/filter.py
@@ -9,10 +9,34 @@ f = open(filename)
         print line,       #comma suppresses new (blank) line """
 
 
-for line in f:
-    fields = line.split()    #splits into columns  
+""" for line in f:
+    fields = line.split()    #splits into separate strings   
     if "tRNA" in fields[9]:  #only prints lines with "tRNA" in gene-name (10th column)
-        print line
+        print line, """
+
+""" # Iterate the file line by line, only lines 11-15 
+line_count = 0
+for data in f:
+    if line_count <= 10:
+        pass    #null operation
+    elif line_count <= 15: 
+        print data, 
+    else: 
+        break   #stops from searching through whole file 
+    line_count += 1  #happens no matter what line_count is """
+    
+line_count = 0
+for line_count, data in enumerate(f):
+    if line_count <= 10:
+        pass
+    elif line_count <= 15:
+        print data, 
+    else: 
+        break 
+    # do not need the incrementing function 
+    
+    
+
 
 
 
