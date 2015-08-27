adb3c4cf53b97564eda05786fa1a55fb843d3fdb
Heather Baldwin <h.baldwin07@gmail.com>
2015-08-27 09:26:03 -0400

    update Q1

--- day3/day3-lunch/dy3lunch1.py
+++ day3/day3-lunch/dy3lunch1.py
@@ -10,13 +10,13 @@ metadf = pd.read_csv("~/qbb2015/rawdata/samples.csv")
 metadf2 = pd.read_csv("~/qbb2015/rawdata/replicates.csv")
 
 
-dev_stage = {}
+"""dev_stage = {}
 for i, data in metadf.iterrows():
     if data["stage"] in ["10", "11", "12", "13", "14A", "14B", "14C", "14D"]:
         if data["stage"] not in dev_stage:
             dev_stage[data["stage"]] = 1
         else:
-            dev_stage[data["stage"]] += 1
+            dev_stage[data["stage"]] += 1"""
 
 
 Sxl = []
@@ -30,7 +30,6 @@ for sample in metadf2[metadf2["sex"] == "female"]["sample"]:
     df2 = pd.read_table("~/qbb2015/stringtie/" + sample + "/t_data.ctab")
     roi2 = df2["t_name"].str.contains("FBtr0331261")
     Sxl2.append(df2[roi2]["FPKM"].values)
-    dev_stage[data["stage"]] += 4
 
 Sxlm = []
 for sample in metadf[metadf["sex"] == "male"]["sample"]:
@@ -43,7 +42,6 @@ for sample in metadf2[metadf2["sex"] == "male"]["sample"]:
     dfm2 = pd.read_table("~/qbb2015/stringtie/" + sample + "/t_data.ctab")
     roim2 = dfm2["t_name"].str.contains("FBtr0331261")
     Sxlm2.append(dfm2[roim2]["FPKM"].values)
-    dev_stage[data["stage"]] += 4
 
 
 print Sxl
@@ -53,11 +51,11 @@ print Sxlm2
 
 plt.figure()
 plt.plot(Sxl, 'r')
-plt.plot(Sxl2, 'y')
+plt.plot([4,5,6,7], Sxl2, 'y')
 plt.plot(Sxlm, 'b')
-plt.plot(Sxlm2, 'g')
+plt.plot([4,5,6,7], Sxlm2, 'g')
 plt.legend(['Females', 'Females rep', 'Males', 'Males rep'])
-plt.xticks(range(len(dev_stage)), ["10", "11", "12", "13", "14A", "14B", "14C", "14D"])
+plt.xticks([0,1,2,3,4,5,6,7], ["10", "11", "12", "13", "14A", "14B", "14C", "14D"])
 plt.ylim([0,300])
 plt.ylabel("FPKM")
 plt.xlabel("Dev Stage")
