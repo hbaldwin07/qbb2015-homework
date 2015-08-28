778f2fb4f08ee794950b64b530e8b73199173052
Heather Baldwin <h.baldwin07@gmail.com>
2015-08-28 07:07:19 -0400

    if A and not B

---
+++ day4/chrombits-updated.py
@@ -0,0 +1,55 @@
+import sys
+import numpy
+import copy
+from __future__ import division
+
+class ChromosomeLocationBitArrays(object):
+    def __init__ (self, fname ):      # storing data in ref
+        arrays = {}     #this is a local variable 
+        for line in open( fname ):
+            fields = line.split()
+            name = fields[0]
+            size = int( fields[1] )
+            arrays[name] = np.zeros(size, dtype=bool) #array of zeros, making spaces to be filled in                                             #dtype - interpretation of array (boolean- 1s and 0s)
+        self.arrays = arrays
+    def set_bits_from_file (self, dicts=None, fname=None): 
+        # if dicts parameter provided, use to initialize
+        if dicts is not None:
+            arrays = dicts
+        else:
+            arrays = {}
+        # if fname parameter provided, initialize from file 
+        if fname is not None:
+            for line in open (fname):
+                fields = line.split() 
+                chrom = fields[0]
+                start = int(fields[1])
+                end = int( fields[2])
+                self.arrays[chrom][start : end] = 1  
+    def intersect (self, other):
+        rval = {}
+        for chrom in self.arrays:
+            rval[chrom] = self.arrays[chrom] | arrays2[chrom]
+        return ChromosomeLocationBitArrays( dicts=rval)
+
+    def union (self, other):
+        rval = {}
+        for chrom in self.arrays:
+            rval[chrom] = arrays1[chrom] & arrays2[chrom]
+        return ChromosomeLocationBitArrays( dicts=rval)
+
+    def complement (arrays):
+        rval = {}
+        for chrom in self.arrays:
+            rval[chrom] = ~ arrays1[chrom]
+        return ChromosomeLocationBitArrays( dicts=rval)
+    
+    def new (self, other):
+        rval = {}
+        for chrom in self.arrays:
+            rval[chrom] = self.arrays[chrom] & not(arrays2[chrom])
+        return ChromosomeLocationBitArrays( dicts = rval)
+            
+    
+    def copy( self):
+        return ChromosomeLocationBitArrays( dicts=copy.deepcopy(self.arrays))
